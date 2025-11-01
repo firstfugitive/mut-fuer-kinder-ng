f99d34bf6676fb986d858ab964422013fe953f8a
firstfugitive <benjacobsen@outlook.de>
2025-11-01 16:29:24 +0100

    remove experiment

--- src/app/pages/page-content-home/page-content-home.ts
+++ src/app/pages/page-content-home/page-content-home.ts
@@ -5,25 +5,13 @@ import { PageHeader } from '../../components/organism/page-header/page-header';
 import { CfPageContentHome } from '../../models/contentful-content-types/page-content';
 import { Asset } from 'contentful';
 import { BaseImage } from "../../components/atom/base-image/base-image";
-import { NgComponentOutlet, NgOptimizedImage } from '@angular/common';
+import { NgComponentOutlet } from '@angular/common';
 import { getImageUrl } from '../../components/shared/utils';
 
 @Component({
   selector: 'app-page-content-home',
-  imports: [PageFooter, PageHeader, BaseImage, NgComponentOutlet, NgOptimizedImage],
-  templateUrl: './page-content-home.html',
-  styles: `
-    .img-wrapper {
-      display: block;
-      position: relative;
-      width: 20rem;
-      height: 500px;
-
-      img {
-        object-fit: cover;
-      }
-    }
-  `
+  imports: [PageFooter, PageHeader, BaseImage, NgComponentOutlet],
+  templateUrl: './page-content-home.html'
 })
 export class PageContentHome extends PageContent {
   override pageContent = input<CfPageContentHome>();
