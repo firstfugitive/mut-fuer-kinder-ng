0ee083f552cfcba73164d58a4e6e26fcd50cd75a
firstfugitive <benjacobsen@outlook.de>
2025-11-01 18:04:52 +0100

    news overview fix

--- src/app/components/molecule/blog-teaser/blog-teaser.ts
+++ src/app/components/molecule/blog-teaser/blog-teaser.ts
@@ -2,13 +2,14 @@ import { Component, computed, input, ViewEncapsulation } from '@angular/core';
 import { CfPage } from '../../../models/contentful-content-types/page';
 import { CfPageContentBlog } from '../../../models/contentful-content-types/page-content';
 import { Asset } from 'contentful';
-import { getUrlFromPage } from '../../shared/utils';
+import { getImageUrl, getUrlFromPage } from '../../shared/utils';
 import { RouterModule } from '@angular/router';
 import { BaseImage } from "../../atom/base-image/base-image";
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-blog-teaser',
-  imports: [RouterModule, BaseImage],
+  imports: [RouterModule, BaseImage, NgOptimizedImage],
   templateUrl: './blog-teaser.html',
   styleUrl: './blog-teaser.scss',
 })
@@ -19,4 +20,6 @@ export class BlogTeaser {
   blogContent = computed<CfPageContentBlog>(() => this.data()?.fields?.content);
   blogTitle = computed<string>(() => this.blogContent().fields?.title);
   blogImage = computed<Asset>(() => this.blogContent().fields?.image);
+  blogImageUrl = computed<string>(() => getImageUrl(this.blogImage()));
+  blogImageTitle = computed<string>(() => this.blogImage()?.fields?.title?.toString());
 }
