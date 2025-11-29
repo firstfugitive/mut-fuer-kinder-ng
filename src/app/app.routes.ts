6038af2bb74e744562bde474678f2c77be7c0b8c
firstfugitive <benjacobsen@outlook.de>
2025-11-29 18:43:30 +0100

    handle errors by forwarding to home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { DynamicPage } from './pages/dynamic-page/dynamic-page';
-import { environment } from '../environments/environment';
 import { generatePaths } from './components/shared/generatePaths';
 
 export const routes: Routes = [
@@ -8,8 +6,8 @@ export const routes: Routes = [
         path: '',
         loadChildren: generatePaths
     },
-    // {
-    //     path: '*',
-    //     redirectTo: 'home'
-    // }
+    {
+        path: '*',
+        redirectTo: ''
+    }
 ];
