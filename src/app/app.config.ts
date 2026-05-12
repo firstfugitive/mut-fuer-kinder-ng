13c350d50f23e8030e9e41a3873994ec28d08415
Ben Jacobsen <benjacobsen@outlook.de>
2026-05-12 13:57:20 +0200

    update: zoneless

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { IMAGE_LOADER, ImageLoaderConfig } from '@angular/common';
+import { IMAGE_LOADER } from '@angular/common';
 import { imageLoaderConfigContentful } from './components/shared/contentful';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })),
     provideClientHydration(withEventReplay()),
     {
